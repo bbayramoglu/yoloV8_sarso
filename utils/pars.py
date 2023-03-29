e5f8f2dfa487e1d4dd129546aeb0ed6a7eb1f59f
bbayramoglu <furkanbbayramoglu@gmail.com>
2023-03-29 23:33:50 +0300

    updated some lines

--- utils/pars.py
+++ utils/pars.py
@@ -1,7 +1,7 @@
 def arg():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w', '--weights', type=str, default="yolov8n.pt", help='choose model')
+    parser.add_argument('-w', '--weights', type=str, default="yolov8n.pt", help=' choose model')
     parser.add_argument('--name', type=str, default="videos", help='change video name')
     parser.add_argument('--socketip', type=str, default="127.0.1.1", help='use for indicate your ip')
     parser.add_argument('-s', '--source', type=str, default="0", help='chose your source')
